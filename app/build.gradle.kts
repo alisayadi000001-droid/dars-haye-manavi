3f5f74751ecfa6b26174e6c4d1a257248ee28103
alisayadi000001-droid <alisayadi000001@gmail.com>
2026-08-19 23:34:29 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,15 +23,16 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  create("debugConfig") {
-  storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
-  storePassword = "android"
-  keyAlias = "androiddebugkey"
-  keyPassword = "android"
-} = System.getenv("KEY_PASSWORD")
+  signingConfigs {
+    create("release") {
+      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+      storeFile = file(keystorePath)
+      storePassword = System.getenv("STORE_PASSWORD")
+      keyAlias = "upload"
+      keyPassword = System.getenv("KEY_PASSWORD")
     }
     create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
+      storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
       storePassword = "android"
       keyAlias = "androiddebugkey"
       keyPassword = "android"
